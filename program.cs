5193433251e69611f23ff813d579fecc8a841f49
Elizaveta <mitrigkkpd@gmail.com>
2023-12-26 15:23:24 +0300

    создание пользовательского (исходного) массива

---
+++ program.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.XPath;
+
+string[] createArray()
+{
+    Console.WriteLine("Введите кол-во строк: ");
+    int numberOfStrings = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[numberOfStrings];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите текст {i + 1} строки: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void printArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+
+string[] array = createArray();
+Console.WriteLine();
+Console.WriteLine("Ваш массив:");
+printArray(array);
