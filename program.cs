aae7d0f2b992cdcf5fd0b76819cac6b06999d3d6
Elizaveta <mitrigkkpd@gmail.com>
2023-12-26 15:28:41 +0300

    добавлена фильтрация исходного массива

--- program.cs
+++ program.cs
@@ -21,8 +21,50 @@ void printArray(string[] array)
     }
 }
 
+string[] Filter(string[] arrayOfStr)
+{
+    string[] resultArray = new string[arrayOfStr.Length];
+    int countOfEmptyStr = 0;
+    for (int i = 0; i < arrayOfStr.Length; i++)
+    {
+        if (arrayOfStr[i].Length < 4)
+        {
+            resultArray[i] = arrayOfStr[i];
+        }
+        else
+        {
+            countOfEmptyStr++;
+        }
+
+    }
+    resultArray = DeleteEmptyElements(resultArray, countOfEmptyStr);
+    return resultArray;
+
+}
+
+string[] DeleteEmptyElements(string[] array, int count)
+{
+    string[] resultArray = new string[array.Length - count];
+    int number = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] != null)
+        {
+            resultArray[number] = array[i];
+            number++;
+        }
+
+    }
+
+    return resultArray;
+}
 
 string[] array = createArray();
 Console.WriteLine();
 Console.WriteLine("Ваш массив:");
 printArray(array);
+Console.WriteLine();
+string[] filterArray = Filter(array);
+Console.WriteLine("Отфильтрованный массив: ");
+// string[] FilterArray= array.Where(a => a.Length <= 3).ToArray(); коллекции
+printArray(filterArray);
